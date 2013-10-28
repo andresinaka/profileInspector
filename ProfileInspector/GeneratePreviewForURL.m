de4ae564fa64478c0fefd755216c30951d46c923
Andres Canal <andres@inakanetworks.com>
2013-10-28 09:48:28 -0300

    Initial commit: created quicklook plugin project, added library to unzip ipa and more

---
+++ ProfileInspector/GeneratePreviewForURL.m
@@ -0,0 +1,31 @@
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <Cocoa/Cocoa.h>
+#include <QuickLook/QuickLook.h>
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
+
+/* -----------------------------------------------------------------------------
+   Generate a preview for file
+
+   This function's job is to create preview for designated file
+   ----------------------------------------------------------------------------- */
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
+{
+    NSLog(@"Testing");
+    
+    NSString *_content = [NSString stringWithContentsOfURL:(__bridge NSURL *)url encoding:NSUTF8StringEncoding error:nil];
+    
+    QLPreviewRequestSetDataRepresentation(preview,(__bridge CFDataRef)[_content dataUsingEncoding:NSUTF8StringEncoding],kUTTypePlainText,NULL);
+    
+    NSLog(@"GeneratePreviewForURL");
+    
+    return noErr;
+}
+
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
+{
+    // Implement only if supported
+}
---
+++ ProfileInspector/GenerateThumbnailForURL.m
@@ -0,0 +1,83 @@
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <QuickLook/QuickLook.h>
+#include <Cocoa/Cocoa.h>
+#include <WebKit/WebKit.h>
+#include "ZipFile.h"
+#include "FileInZipInfo.h"
+#include "ZipReadStream.h"
+
+OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
+void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
+
+/* -----------------------------------------------------------------------------
+    Generate a thumbnail for file
+
+   This function's job is to create thumbnail for designated file as fast as possible
+   ----------------------------------------------------------------------------- */
+
+OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
+{
+ 
+    ZipFile *unzipFile= [[ZipFile alloc] initWithFileName:@"app.ipa"
+                                                     mode:ZipFileModeUnzip];
+
+    [unzipFile locateFileInZip:@"Payload/spotmyride.app/embedded.mobileprovision"];
+    ZipReadStream *read= [unzipFile readCurrentFileInZip];
+    
+    
+    NSMutableData *data= [[NSMutableData alloc] initWithLength:9092];
+    long bytesRead = [read readDataWithBuffer:data];
+    [read finishedReading];
+    [unzipFile close];
+    
+    NSLog(@"Data Read: %ld",bytesRead);
+
+    
+    NSString *dataStr;
+    
+    
+    NSString *newStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", [data description]);
+    
+    /*
+    NSString *_content = [NSString stringWithContentsOfURL:(__bridge NSURL *)url encoding:NSUTF8StringEncoding error:nil];
+
+    if (_content) {
+        // Encapsulate the content of the .strings file in HTML
+        NSString *_html = [NSString stringWithFormat:@"<html><body><pre>%@</pre></body></html>", _content];
+        NSData *_data   = [_html dataUsingEncoding:NSUTF8StringEncoding];
+        
+		NSRect _rect = NSMakeRect(0.0, 0.0, 600.0, 800.0);
+		float _scale = maxSize.height / 800.0;
+		NSSize _scaleSize = NSMakeSize(_scale, _scale);
+		CGSize _thumbSize = NSSizeToCGSize((CGSize) { maxSize.width * (600.0/800.0), maxSize.height});
+        
+        // Create the webview to display the thumbnail
+        WebView *_webView = [[WebView alloc] initWithFrame:_rect];
+		[_webView scaleUnitSquareToSize:_scaleSize];
+        [_webView.mainFrame.frameView setAllowsScrolling:NO];
+        [_webView.mainFrame loadData:_data MIMEType:@"text/html" textEncodingName:@"utf-8" baseURL:nil];
+        
+		while([_webView isLoading]) CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true);
+        [_webView display];
+        
+        // Draw the webview in the correct context
+		CGContextRef _context = QLThumbnailRequestCreateContext(thumbnail, _thumbSize, false, NULL);
+		if (_context) {
+			NSGraphicsContext* _graphicsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:(void *)_context flipped:_webView.isFlipped];
+			[_webView displayRectIgnoringOpacity:_webView.bounds inContext:_graphicsContext];
+			QLThumbnailRequestFlushContext(thumbnail, _context);
+			CFRelease(_context);
+		}
+    }
+    
+    NSLog(@"GenerateThumbnailForURL");
+    */
+    return noErr;
+}
+
+void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail)
+{
+    // Implement only if supported
+}
